8974f147bea3c39d61bcdbf6e11c6c30b0a3b383
Pralay Mondal <poluchamp01@gmail.com>
2022-06-20 21:40:13 +0530

    Included button animations

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    makeAnimation(buttonInnerHTML);
 
   });
 }
 
 document.addEventListener("keypress", function(event) {
   makeSound(event.key);
+  makeAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -47,4 +49,12 @@ function makeSound(key) {
       alert("Wrong Input Detected !");
 
   }
+}
+
+function makeAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
